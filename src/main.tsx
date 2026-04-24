586d719a94a353a76d38f5b2d3cd8ebb5856bab4
Rhidaa Omar <orhidaa@gmail.com>
2026-04-24 17:17:29 +0000

    Remove Quirkify boot recovery reload loop

--- src/main.tsx
+++ src/main.tsx
@@ -4,24 +4,6 @@ import App from './App.tsx';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-async function clearLegacyPwaState() {
-  if ('serviceWorker' in navigator) {
-    const registrations = await navigator.serviceWorker.getRegistrations();
-    await Promise.all(registrations.map((registration) => registration.unregister()));
-  }
-
-  if ('caches' in window) {
-    const cacheKeys = await caches.keys();
-    await Promise.all(
-      cacheKeys
-        .filter((key) => key.includes('workbox') || key.includes('quirkify-'))
-        .map((key) => caches.delete(key)),
-    );
-  }
-}
-
-void clearLegacyPwaState();
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
