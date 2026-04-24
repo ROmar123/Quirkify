e20a62e61165c67b11458a07aa6a95e3042881d0
Rhidaa Omar <orhidaa@gmail.com>
2026-04-24 16:06:14 +0000

    Serve cleanup service worker at root

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(Promise.resolve());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    (async () => {
+      const cacheKeys = await caches.keys();
+      await Promise.all(cacheKeys.map((key) => caches.delete(key)));
+      await self.registration.unregister();
+      const clients = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+      await Promise.all(clients.map((client) => client.navigate(client.url)));
+    })(),
+  );
+});
